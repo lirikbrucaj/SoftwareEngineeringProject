3bca8fa294fbd12dbec1f55edc90bfcb5f73c99d
Lirik <Lirik.brucaj@gmail.com>
2019-10-02 11:08:03 -0400

    First commit with login information

---
+++ app/src/main/java/lirik/myapplication/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package lirik.myapplication.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import lirik.myapplication.data.LoginDataSource;
+import lirik.myapplication.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
